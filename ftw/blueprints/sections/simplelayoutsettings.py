22bdd1ed9cc4d179e14d6c864a89d9dd2a30bfb5
Timon Tschanz <timon.tschanz@4teamwork.ch>
2013-08-12 15:38:24 +0200

    add simplelayoutsetting section

---
+++ ftw/blueprints/sections/simplelayoutsettings.py
@@ -0,0 +1,38 @@
+from collective.transmogrifier.interfaces import ISection
+from collective.transmogrifier.interfaces import ISectionBlueprint
+from simplelayout.base.interfaces import ISimpleLayoutBlock
+from simplelayout.base.interfaces import ISimpleLayoutCapable
+from simplelayout.base.interfaces import ISlotA
+from simplelayout.base.interfaces import ISlotB
+from collective.transmogrifier.utils import Expression
+from zope.interface import classProvides, implements
+import os
+from zope.interface import directlyProvides, noLongerProvides
+from simplelayout.base.interfaces import ISimplelayoutTwoColumnView
+from zope.annotation.interfaces import IAnnotations
+
+class SimplelayoutSettings(object):
+    """Baseclass to add an item to the transmogrifier pipeline.
+    """
+    classProvides(ISectionBlueprint)
+    implements(ISection)
+
+    def __init__(self, transmogrifier, name, options, previous):
+        self.previous = previous
+        self.context = transmogrifier.context
+        self.sitepath = options['sitepath']
+
+    def __iter__(self):
+        for item in self.previous:
+            obj = self.context.restrictedTraverse(self.sitepath + item['_path'])
+            if ISimpleLayoutCapable.providedBy(obj):
+                if item['two_columns']:
+                    directlyProvides(obj, ISimplelayoutTwoColumnView)
+            elif ISimpleLayoutBlock.providedBy(obj):
+                if item['imageLayout']:
+                    anno = IAnnotations(obj)
+                    anno['imageLayout'] = item['imageLayout']
+                if item['right_slot']:
+                    noLongerProvides(obj, ISlotA)
+                    directlyProvides(obj, ISlotB)
+            yield item
