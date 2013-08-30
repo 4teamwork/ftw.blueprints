114d09db364fd13509bdf26042d1497bf538d17d
Timon Tschanz <timon.tschanz@4teamwork.ch>
2013-08-30 16:28:53 +0200

    upate portletinserter to work for me

--- ftw/blueprints/sections/portlet.py
+++ ftw/blueprints/sections/portlet.py
@@ -10,35 +10,40 @@ from zope.component import getMultiAdapter
 from zope.component import getUtility
 from zope.interface import classProvides, implements
 import logging
-
+from zope.dottedname.resolve import resolve
 
 class PortletHandler(object):
 
-    def __init__(self, manager_name, assignment_path, portlet_id):
-        self.manager_name = manager_name
-        self.portlet_id = portlet_id
-        self.assignment_path = assignment_path
 
     def __call__(self, context, properties=None):
         properties = properties or {}
-        manager = getUtility(IPortletManager, name=self.manager_name)
+        manager = getUtility(IPortletManager, name=properties['manager'])
         assignment_mapping = getMultiAdapter(
             (context, manager), IPortletAssignmentMapping)
 
         # Set new portlet assignment
-        if self.portlet_id in assignment_mapping:
+        name = properties['__dict__']['__name__']
+        del properties['__dict__']['__name__']
+
+        if name in assignment_mapping:
             return
 
-        assignment_mapping[self.portlet_id] = self.get_assignment_object(
+        assignment_mapping[name] = self.get_assignment_object(
             properties)
 
     def get_assignment_object(self, properties=None):
         """Return the assignment object of the defined portlet
         """
         properties = properties or {}
-        assignment_class = self._get_assignment_class(self.assignment_path)
+        class_ = properties['class']
+        if class_ == 'ftw.portlet.staticTextExtended.static.Assignment':
+            class_ = 'plone.portlet.static.static.Assignment'
+            del properties['__dict__']['hide']
+            del properties['__dict__']['show_title']
+            del properties['__dict__']['actual_context']
 
-        return assignment_class(**properties)
+        assignment_class = resolve(class_)
+        return assignment_class(**properties['__dict__'])
 
     def _get_assignment_class(self, assignment_path):
         """Try to import and return the defined assignment class
@@ -62,18 +67,10 @@ class ContextualPortletAdder(object):
 
         self.pathkey = options.get('path-key', '_path')
 
-        self.portlet_properties = Expression(
-            options.get('portlet-properties', 'python:{}'),
-            transmogrifier, name, options)
-
         self.condition = Condition(options.get('condition', 'python:True'),
             transmogrifier, name, options)
 
-        self.portlet_handler = PortletHandler(
-            options.get('manager-name'),
-            options.get('assignment-path'),
-            options.get('portlet-id')
-        )
+        self.portlet_handler = PortletHandler()
 
     def __iter__(self):
         for item in self.previous:
@@ -88,8 +85,8 @@ class ContextualPortletAdder(object):
                     "Context does not exist at %s" % item.get[self.pathkey])
                 yield item
                 continue
-
-            self.portlet_handler(obj, self.portlet_properties(item))
+            for portlet in item['portlets']:
+                self.portlet_handler(obj, portlet)
 
             self.logger.info(
                 "Added portlet at %s" % (item[self.pathkey]))
