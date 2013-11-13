e9f649b9349cc8ebdced48bf1e94a00269bb878f
Timon Tschanz <timon.tschanz@4teamwork.ch>
2013-11-13 15:29:59 +0100

    add position updater.

--- ftw/blueprints/sections/updater.py
+++ ftw/blueprints/sections/updater.py
@@ -1,7 +1,10 @@
+from Products.Archetypes.interfaces import IBaseObject
 from collective.transmogrifier.interfaces import ISection
 from collective.transmogrifier.interfaces import ISectionBlueprint
 from collective.transmogrifier.utils import Condition
-from Products.Archetypes.interfaces import IBaseObject
+from collective.transmogrifier.utils import defaultMatcher
+from plone.folder.default import DefaultOrdering
+from zope.annotation.interfaces import IAnnotations
 from zope.interface import classProvides, implements
 import base64
 import logging
@@ -76,3 +79,58 @@ class DataUpdater(object):
         extension = extension[1:]
         return self.context.mimetypes_registry.extensions.get(
             extension.lower(), default)
+
+
+class PositionInParentUpdater(object):
+    classProvides(ISectionBlueprint)
+    implements(ISection)
+
+    def __init__(self, transmogrifier, name, options, previous):
+        self.previous = previous
+        self.context = transmogrifier.context
+        self.pathkey = defaultMatcher(options, 'path-key', name, 'path')
+        self.positionkey = defaultMatcher(options, 'position-key', name, '_gopip')
+
+    def __iter__(self):
+        for item in self.previous:
+            keys = item.keys()
+            pathkey = self.pathkey(*keys)[0]
+            positionkey = self.positionkey(*keys)[0]
+            path = item[pathkey]
+            position = item.get(positionkey)
+
+            obj = self.context.unrestrictedTraverse(
+                str(path).lstrip('/'), None)
+            if obj is None:         # path doesn't exist
+                yield item; continue
+
+            self.updateObjectPosition(obj, position)
+
+    def updateObjectPosition(self, obj, position):
+        """
+        Updates the position of the object and it siblings (reset position of
+        all children of the parent object).
+        Objects, which do not exist at the sender instance, are moved to the
+        bottom.
+        """
+        parent = obj.aq_inner.aq_parent
+        annotations = IAnnotations(parent)
+        order = annotations.get(DefaultOrdering.ORDER_KEY)
+        if not order:
+            return
+        order.remove(obj.getId())
+        order.insert(position, obj.getId())
+
+        annotations[DefaultOrdering.ORDER_KEY] = order
+
+        pos = annotations.get(DefaultOrdering.POS_KEY)
+        for n, id in enumerate(order):
+            pos[id] = n
+
+        # reindex all objects
+        for id in order:
+            try:
+                parent.get(id).reindexObject(
+                    idxs=['positionInParent', 'getObjPositionInParent'])
+            except:
+                print "FAILED to indec %s" % id
